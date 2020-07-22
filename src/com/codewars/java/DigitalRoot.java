0ff821bc0abeeebfa8af04403003ff70db782116
Maksym Kazarin <kazzarin99@gmail.com>
2020-07-22 10:45:09 +0300

    optimized version for DigitalRoot was added

--- src/com/codewars/java/DigitalRoot.java
+++ src/com/codewars/java/DigitalRoot.java
@@ -6,6 +6,7 @@ public class DigitalRoot {
 
     public static void main(String[] args) {
         System.out.println(digital_root(942));
+        System.out.println(optimized_digital_root(942));
     }
 
     public static int digital_root(int n) {
@@ -20,5 +21,9 @@ public class DigitalRoot {
         } else {
             return result;
         }
+    }
+
+    public static int optimized_digital_root(int n) {
+        return (n != 0 && n%9 == 0) ? 9 : n % 9;
     }
 }
