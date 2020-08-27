c788e73ae338fb8974f3c9b1ff8ac034b14ebca6
Maksym Kazarin <kazzarin99@gmail.com>
2020-08-27 09:38:20 +0300

    SquareEveryDigit kata was added

---
+++ src/com/codewars/java/SquareEveryDigit.java
@@ -0,0 +1,22 @@
+package com.codewars.java;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class SquareEveryDigit {
+
+    //https://www.codewars.com/kata/546e2562b03326a88e000020
+
+    public static void main(String[] args) {
+        System.out.println(new SquareEveryDigit().squareDigits(9119 ));
+    }
+
+    public int squareDigits(int n) {
+        return Arrays.stream(String.valueOf(n).split(""))
+                .mapToDouble(Double::parseDouble)
+                .mapToObj(d -> String.valueOf(Math.round(Math.pow(d, 2))))
+                .collect(Collectors.collectingAndThen(Collectors.joining(), Integer::parseInt));
+    }
+
+
+}
