cce84f8931221aa57b8b98bcffd7047026467b6f
Maksym Kazarin <kazzarin99@gmail.com>
2020-08-27 09:56:01 +0300

    squareDigitsEasier for SquareEveryDigit was added

--- src/com/codewars/java/SquareEveryDigit.java
+++ src/com/codewars/java/SquareEveryDigit.java
@@ -8,13 +8,20 @@ public class SquareEveryDigit {
     //https://www.codewars.com/kata/546e2562b03326a88e000020
 
     public static void main(String[] args) {
-        System.out.println(new SquareEveryDigit().squareDigits(9119 ));
+        System.out.println(new SquareEveryDigit().squareDigitsEasier(9119 ));
     }
 
     public int squareDigits(int n) {
         return Arrays.stream(String.valueOf(n).split(""))
                 .mapToDouble(Double::parseDouble)
                 .mapToObj(d -> String.valueOf(Math.round(Math.pow(d, 2))))
+                .collect(Collectors.collectingAndThen(Collectors.joining(), Integer::parseInt));
+    }
+
+    public int squareDigitsEasier(int n) {
+        return Arrays.stream(String.valueOf(n).split(""))
+                .mapToInt(Integer::parseInt)
+                .mapToObj(i -> String.valueOf(i * i))
                 .collect(Collectors.collectingAndThen(Collectors.joining(), Integer::parseInt));
     }
 
